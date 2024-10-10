e14bd0cd35885c7d26b131945b6ff07467d88aac
Nicoli Siqueira <nicoli.siqueira@sap.com>
2024-10-10 11:27:49 -0300

    Criei a branch e implementei a lógica na classe main para que o sistema inicie os dados com pelo menos 5 departamentos

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,6 +19,28 @@ public class Main {
 
         appController.setUsuarioAtual(funcionario2);
         appController.exibirUsuarioAtual();
+
+        List<Departamento> departamentos = inicializarDepartamentos();
+        exibirDepartamentos(departamentos);
+    }
+
+    private static List<Departamento> inicializarDepartamentos() {
+        List<Departamento> departamentos = new ArrayList<>();
+
+        departamentos.add(new Departamento("Financeiro", 5000.00));
+        departamentos.add(new Departamento("Engenharia", 10000.00));
+        departamentos.add(new Departamento("RH", 4000.00));
+        departamentos.add(new Departamento("Manutenção", 3000.00));
+        departamentos.add(new Departamento("TI", 7000.00));
+
+        return departamentos;
+    }
+
+    private static void exibirDepartamentos(List<Departamento> departamentos) {
+        System.out.println("Departamentos iniciais:");
+        for (Departamento dep : departamentos) {
+            System.out.println("Nome: " + dep.getNome() + " | Valor Máximo: " + dep.getValorMaximo());
+        }
     }
 }
 
